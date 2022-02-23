6928b070c802eeb703aca1d1a8d00d094ceff2ca
evang-esc <evangivaldosc@gmail.com>
2022-02-23 18:36:33 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgrma.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+/**
+ * @author evang-esc
+ *
+ */
+public class PrimeiroProgrma {
+
+	/**
+	 * 
+	 */
+	public PrimeiroProgrma() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int = 2;
+		int = 3;
+		System.out.println("Hello Word! " + (a+b));
+*/
+	}
+
+}
